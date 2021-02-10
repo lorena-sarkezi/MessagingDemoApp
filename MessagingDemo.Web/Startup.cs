5e54377b0695df62cd69e4e2d69d9459e4ed8a33
lorena-sarkezi <lorena.sarkezi@gmail.com>
2021-02-10 22:29:39 +0100

    Added React app to .net app

--- MessagingDemo.Web/Startup.cs
+++ MessagingDemo.Web/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 
 using MessagingDemo.Data;
 using Microsoft.EntityFrameworkCore;
@@ -29,11 +30,25 @@ namespace MessagingDemo.Web
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
             services.AddDbContext<DemoDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DbConnectionString")));
+
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ReactApp/build";
+            });
+
+            services.AddCors(o => o.AddPolicy("CorsAllowAll", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("CorsAllowAll");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -46,16 +61,33 @@ namespace MessagingDemo.Web
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSpaStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthorization();
+
+            
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.Map("/react", react =>
+            {
+                react.UseSpa(spa =>
+                {
+                    spa.Options.SourcePath = "ReactApp";
+
+                    if (env.IsDevelopment())
+                    {
+                        spa.UseReactDevelopmentServer(npmScript: "start");
+                    }
+
+                });
             });
         }
     }
